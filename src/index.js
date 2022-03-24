9a1cf33f1c86a531520e5b7907660151d2e22fee
Scott Ball <scottgball@gmail.com>
2022-03-24 07:15:19 -0700

    Updated styles and added View Tasks button functionality

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ const projectDisplayControl = (function () {
     console.table(projects);
     const sideBar = document.querySelector('#sideBar');
     const newProjectCard = document.createElement('div');
+    newProjectCard.classList.add('newProjectCard')
     const daysUntilProjectDue = newProject.getDatesUntilProjectDue(newProject.projectDueDate);
     newProjectCard.textContent = `${newProject.projectName}, 
     ${newProject.projectDescription}, 
@@ -55,12 +56,17 @@ const projectDisplayControl = (function () {
     addTaskButton.classList.add('addTaskButton');
     newProjectCard.appendChild(addTaskButton);
     taskButtons.push(addTaskButton);
+    const viewTasksButton = document.createElement('button');
+    viewTasksButton.textContent = `View Tasks`;
+    viewTasksButton.classList.add('addTaskButton');
+    newProjectCard.appendChild(viewTasksButton);
     const taskForm = document.querySelector('#taskForm');
     addTaskButton.addEventListener('click', (e) => {
       taskForm.classList.remove('taskFormHidden');
       taskForm.classList.add('taskFormDisplayed');
       projectIndex = taskButtons.indexOf(addTaskButton);
     });
+    
     taskForm.onsubmit = () => {
       createNewTask();
       taskForm.classList.remove('taskFormDisplayed');
@@ -81,10 +87,11 @@ const projectDisplayControl = (function () {
       const newTask = taskFactory(document.getElementById('taskName').value, document.getElementById('taskDescription').value, document.getElementById('taskDueDate').value, taskUrgency, document.getElementById('taskCompletedOrNot'));
       const contentContainer = document.querySelector('#content');
       const newTaskCard = document.createElement('div');
+      newTaskCard.classList.add('newTaskCard');
       const daysUntilTaskDue = newTask.getDatesUntilTaskDue(newTask.taskDueDate);
       newTaskCard.textContent = `${newTask.taskName},
       ${newTask.taskDescription}, 
-      task due in ${daysUntilTaskDue} days, 
+      due in ${daysUntilTaskDue} days, 
       ${taskUrgency}, 
       Task Incomplete`;
       contentContainer.appendChild(newTaskCard);
@@ -92,8 +99,37 @@ const projectDisplayControl = (function () {
       event.preventDefault();
       console.table(projects);
     };
+
+    const contentContainer = document.querySelector('#content');
+    viewTasksButton.addEventListener('click', (e) => {
+      clearContentContainer();
+      displayTasks();
+    });
+
+    const clearContentContainer = () => {
+      while (contentContainer.firstChild) {
+        contentContainer.removeChild(contentContainer.lastChild);
+      }
+    };
+
+    const displayTasks = () => {
+      newProject.tasks.forEach((task) => {
+        console.log(task);
+        const newTaskCard = document.createElement('div');
+        newTaskCard.classList.add('newTaskCard');
+        const daysUntilTaskDue = task.getDatesUntilTaskDue(task.taskDueDate);
+        newTaskCard.textContent = `${task.taskName},
+        ${task.taskDescription}, 
+        due in ${daysUntilTaskDue} days, 
+        ${taskUrgency}, 
+        Task Incomplete`;
+        contentContainer.appendChild(newTaskCard);
+      });
+      // contentContainer.appendChild(newTaskCard)
+    };
     
     sideBar.appendChild(newProjectCard);
+    
     event.preventDefault();
     
   };
